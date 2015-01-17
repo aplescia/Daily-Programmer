0f6677a4df470a1492fc189e3f497a307f4dfc84
aplescia <anthony.plescia@mail.mcgill.ca>
2015-01-16 23:59:54 -0500

    added poop

---
+++ challengeone.pl
@@ -0,0 +1,18 @@
+#perl program
+
+print "What's your name?\n";
+
+$name = <>;
+
+print "What's your Reddit Username?\n";
+
+$username = <>;
+
+print "What's your age?";
+
+$age = <>;
+
+
+
+poop
+print "Your name is $name, your age is $age, and your username is $username!";
---
+++ challengetwo.py
@@ -0,0 +1,17 @@
+def Calculate(M, A):
+    F = int(M) * int(A)
+    print "The Force is" + " " + str(F)
+
+
+
+
+
+print "Mass?"
+
+mass = raw_input()
+
+print "Acceleration?"
+
+accel = raw_input()
+
+Calculate(mass, accel)
